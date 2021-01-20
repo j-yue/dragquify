22bb4624cd08a3aaeb599c76a420bab80f4d4526
Jeanette Yue <yue.jeanette@gmail.com>
2021-01-19 20:26:32 -0800

    Create VideoUrl component

---
+++ src/components/editor-ui/VideoUrl.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { TextField, Icon, Stack, Button } from "@shopify/polaris";
+import { CircleCancelMajor } from "@shopify/polaris-icons";
+
+export default function VideoUrl() {
+  return (
+    <Stack>
+      <Stack.Item fill>
+        <TextField placeholder="Paste link or search" />
+      </Stack.Item>
+      <Stack.Item>
+        <Button disabled>
+          <Icon source={CircleCancelMajor} />
+        </Button>
+      </Stack.Item>
+    </Stack>
+  );
+}
