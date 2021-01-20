16036d40c84a69c7e7bac604c984a1225573bfe5
Jeanette Yue <yue.jeanette@gmail.com>
2021-01-19 20:35:48 -0800

    Url should accept a prop, value

--- src/components/editor-ui/Url.js
+++ src/components/editor-ui/Url.js
@@ -2,11 +2,11 @@ import React from "react";
 import { TextField, Icon, Stack, Button } from "@shopify/polaris";
 import { CircleCancelMajor } from "@shopify/polaris-icons";
 
-export default function Url() {
+export default function Url({ value }) {
   return (
     <Stack>
       <Stack.Item fill>
-        <TextField placeholder="Paste link or search" />
+        <TextField placeholder="Paste link or search" value={value} />
       </Stack.Item>
       <Stack.Item>
         <Button disabled>
