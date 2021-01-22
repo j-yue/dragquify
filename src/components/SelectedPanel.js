947ec3a80953c90191cc2838f39d7c347dbc7469
Jeanette Yue <yue.jeanette@gmail.com>
2021-01-21 16:23:59 -0800

    Create SelectedPanel component
    - Display component selected from Preview panel with textfields users
     can change
    - Delete and Output actions not implemented

---
+++ src/components/SelectedPanel.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Card, TextField } from "@shopify/polaris";
+
+//render the input selected from the preview tab and
+//give each key besides "tag" a label and input field that user can change
+export default function SelectedPanel({ handleTextChange, selectedInput }) {
+  let tag, otherKeys;
+  //will not be rendering tag key because its value should be constant
+  selectedInput
+    ? ([tag, ...otherKeys] = Object.keys(selectedInput))
+    : ([tag, ...otherKeys] = []);
+
+  return (
+    <Card
+      title="Selected"
+      actions={[
+        {
+          content: "Delete",
+          destructive: true,
+          onAction: () => console.log("deleting"),
+        },
+        {
+          content: "Output",
+          onAction: () => console.log("showing output"),
+        },
+      ]}
+    >
+      {selectedInput && (
+        <Card.Section>
+          {otherKeys.map((key) => {
+            return (
+              <TextField
+                label={key}
+                value={selectedInput[key]}
+                onChange={(e) => handleTextChange(key, e)}
+              />
+            );
+          })}
+        </Card.Section>
+      )}
+    </Card>
+  );
+}
