6deab89d4466c5b01788af3ca98828563ff5e8bf
Jeanette Yue <yue.jeanette@gmail.com>
2021-01-23 18:12:12 -0800

    - Use hooks to keep track of inputs added and input selected - Use hooks to create handlers for state hooks to pass to PreviewPanel, SelectedPanel, and SettingsPanel - Refactor preview portion with PreviewPanel component

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AppProvider, Card } from "@shopify/polaris";
 import "@shopify/polaris/dist/styles.css";
 import SettingsPanel from "./SettingsPanel";
-
-import { ListInput, FontPicker, ImagePicker } from "./editor-ui/listInputs";
-import Richtext from "./editor-ui/Richtext";
-import VideoUrl from "./editor-ui/VideoUrl";
-import Url from "./editor-ui/Url";
-import Header from "./editor-ui/Header";
-import Paragraph from "./editor-ui/Paragraph";
+import PreviewPanel from "./PreviewPanel";
+import SelectedPanel from "./SelectedPanel";
 
 export default function App() {
+  // inputs added from SettingsPanel
+  const [inputs, setInputs] = useState([]);
+
+  // index of input selected from PreviewPanel
+  const [selectedInput, setSelectedInput] = useState(null);
+
+  // input selected from PreviewPanel
+  const [selectedInputIndex, setSelectedInputIndex] = useState(0);
+
+  // ref for PreviewPanel
+  const previewRef = React.createRef();
+
+  const handleInputChange = (inputs) => setInputs(inputs);
+
+  // onClick handler for SettingsPanel
+  const addInput = (input) => handleInputChange([...inputs, input]);
+
+  // onClick handler for PreviewPanel
+  const handlePreviewClick = (index) => {
+    setSelectedInputIndex(index);
+  };
+
+  // onChange handler for SelectedPanel
+  const handleTextChange = (key, value) => {
+    let updatedInput = Object.assign({}, selectedInput);
+    updatedInput[key] = value;
+    let updatedInputs = [...inputs];
+    updatedInputs[selectedInputIndex] = updatedInput;
+    setInputs(updatedInputs);
+  };
+
+  // preview panel
+  // whenever input added, preview panel should scroll to it
+  useEffect(() => {
+    if (inputs.length)
+      previewRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
+  }, [inputs]);
+
+  // selected panel
+  // rerender whenever a new rendered input is clicked
+  useEffect(() => {
+    console.log(`index is ${selectedInputIndex}`);
+    setSelectedInput(inputs[selectedInputIndex]);
+  }, [selectedInputIndex, inputs]);
+
   return (
     <AppProvider>
       <div className="grid">
         <h1 className="grid__header">dragquify</h1>
         <div className="app">
           <div className="app__selected">
-            <Card title="Selected">
-              <p>selected</p>
-            </Card>
+            <SelectedPanel
+              handleTextChange={handleTextChange}
+              selectedInput={selectedInput}
+            />
           </div>
+
           <div className="app__preview">
             <Card title="Preview">
-              <Header name="Body Styles" info="" />
-              <Header name="Body Styles" info="everthing" />
-              <Header name="Body Styles" info="everthing" />
-              <Header name="Body Styles" info="everthing" />
-              <Paragraph text="All of your collections are listed by default. To customize your list, choose 'Selected' and add collections." />
-              <Paragraph text="All of your collections are listed by default. To customize your list, choose 'Selected' and add collections." />
-              <Header name="Body Styles" info="everthing" />
-              <Header name="Body Styles" info="everthing" />
-              <Paragraph text="All of your collections are listed by default. To customize your list, choose 'Selected' and add collections." />
-              <Paragraph text="All of your collections are listed by default. To customize your list, choose 'Selected' and add collections." />
-
-              <Url value="crochetboss.com" />
-
-              <VideoUrl value="https://www.youtube.com/watch?v=_9VUPq3SxOc" />
-              <Richtext value="123 somewhere place, 91210" />
-              <ListInput label="Select collection" />
-              <ListInput label="Select article" />
-              <ListInput label="Select product" />
-              <ListInput label="Select blog" />
-              <ListInput label="Select page" />
-              <ListInput label="Select menu" />
-              <ImagePicker />
-              <FontPicker font="BIZ UDPGothic" style="bold" />
-              {/* <ColorPicker color="#066465" label="Background Color" /> */}
+              <PreviewPanel
+                inputs={inputs}
+                previewRef={previewRef}
+                handleClick={handlePreviewClick}
+              />
             </Card>
           </div>
           <div className="app__settings">
-            <SettingsPanel />
+            <SettingsPanel addInput={addInput} />
           </div>
         </div>
       </div>
