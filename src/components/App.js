543fe0cfebbf74761aacf30dece8ded47d2a0c72
Jeanette Yue <yue.jeanette@gmail.com>
2021-02-12 09:28:11 -0800

    Add previewRef as missing dep

--- src/components/App.js
+++ src/components/App.js
@@ -46,7 +46,7 @@ export default function App() {
         block: "start",
         inline: "nearest",
       });
-  }, [inputs]);
+  }, [inputs, previewRef]);
 
   // selected panel
   // rerender whenever a new rendered input is clicked
