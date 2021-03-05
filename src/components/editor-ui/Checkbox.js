7ed96bbe2973410dcede5020aaad3925e47a465e
Jeanette Yue <yue.jeanette@gmail.com>
2021-03-04 17:30:49 -0800

    Add padding to info under checkbox

--- src/components/editor-ui/Checkbox.js
+++ src/components/editor-ui/Checkbox.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Checkbox as PolarisCheckbox } from "@shopify/polaris";
 
+import "./Checkbox.css";
+
 export default function Checkbox({ ...props }) {
   return (
     <PolarisCheckbox
